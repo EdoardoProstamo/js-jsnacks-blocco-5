9d91d0808e68a242ee200799c6abe4eb13633e72
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-17 01:11:38 +0100

    feat: snack-6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,10 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+
+const zucchine15oPiu = zucchine.filter((zucchina) => zucchina.length >= 15);
+console.log(zucchine15oPiu);
+
+const zucchineMenoDi15 = zucchine.filter((zucchina) => zucchina.length < 15);
+console.log(zucchineMenoDi15);
